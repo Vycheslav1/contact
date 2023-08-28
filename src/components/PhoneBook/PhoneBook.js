228c798c23fd927dc4797bcfc9360e70ab1de1ae
Vyacheslav Stetsenko <slavastetsenko@gmail.com>
2023-08-29 00:14:26 +0300

    project

--- src/components/PhoneBook/PhoneBook.js
+++ src/components/PhoneBook/PhoneBook.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 import { Div, Title, ContactsTitle } from './PhoneBookStyles.js';
 
@@ -17,22 +17,22 @@ const PhoneBook = () => {
 
   let changedItems = [];
 
-  let items = JSON.parse(localStorage.getItem('phonebook'));
+  let items = useRef(JSON.parse(localStorage.getItem('phonebook')));
 
   useEffect(() => {
-    items = JSON.parse(localStorage.getItem('phonebook'));
+    items.current = JSON.parse(localStorage.getItem('phonebook'));
   }, [contacts]);
 
   changedItems = [...items];
 
   if (filter !== '') {
-    changedItems = items.filter(item => item.name.includes(filter));
+    changedItems = items.current.filter(item => item.name.includes(filter));
   }
 
   return (
     <Div>
       <Title>Phonebook</Title>
-      <ContactForm stateSubmit={setContacts} persons={items} />
+      <ContactForm stateSubmit={setContacts} persons={items.current} />
       <ContactsTitle>Contacts</ContactsTitle>
       <Filter changeState={setFilter} />
       <ContactList persons={changedItems} changeList={setContacts} />
